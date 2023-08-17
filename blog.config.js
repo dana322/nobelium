b09bc69713a468221d5d2372cff6eb81deaaa87f
danan <sbzddanan@gmail.com>
2023-08-17 11:42:44 +0800

    add comment and modify title

--- blog.config.js
+++ blog.config.js
@@ -1,5 +1,5 @@
 const BLOG = {
-  title: 'CRAIGARY',
+  title: "Dana' blog",
   author: 'dana',
   email: 'sbzddanan@gmail.com',
   link: 'https://nobelium.vercel.app',
@@ -38,7 +38,7 @@ const BLOG = {
   },
   comment: {
     // support provider: gitalk, utterances, cusdis
-    provider: '', // leave it empty if you don't need any comment plugin
+    provider: 'cusdis', // leave it empty if you don't need any comment plugin
     gitalkConfig: {
       repo: '', // The repository of store comments
       owner: '',
@@ -51,7 +51,7 @@ const BLOG = {
       repo: ''
     },
     cusdisConfig: {
-      appId: '', // data-app-id
+      appId: 'a3deaf32-1fea-40de-9990-836ad3093cf3', // data-app-id
       host: 'https://cusdis.com', // data-host, change this if you're using self-hosted version
       scriptSrc: 'https://cusdis.com/js/cusdis.es.js' // change this if you're using self-hosted version
     }
